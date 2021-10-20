6b1bd02e6c97be7e9a6085bd21011edcac2cdfaf
Liu Cypher <antoine-bodin@hotmail.fr>
2021-10-20 14:34:59 +0200

    day 2 exercise 2

---
+++ day-2/exercise-2.js
@@ -0,0 +1,12 @@
+export const my_display_multi_42 = (multi, int) => {
+  return multi * int === 42;
+}
+
+/*
+console.log(my_display_multi_42(21, 2));
+console.log(my_display_multi_42(24));
+console.log(my_display_multi_42());
+console.log(my_display_multi_42(undefined, 2));
+console.log(my_display_multi_42(null));
+console.log(my_display_multi_42(null, null));
+*/
