3903f3cf1f5df91695ab9c53172f092afa4399ec
Liu Cypher <antoine-bodin@hotmail.fr>
2021-10-20 14:21:08 +0200

    day 2 exercise 0

---
+++ day-2/exercise-0.js
@@ -0,0 +1,12 @@
+export const my_display_forever_42 = (nbr) => {
+  if (typeof(nbr) == "number") {
+    return nbr;
+  }
+  else {
+    return 42;
+  }
+}
+
+/*
+console.log(my_display_forever_42());
+*/
