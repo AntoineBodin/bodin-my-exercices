9b7b1c173906ea526361b35118d3157ee13c7b02
Liu Cypher <antoine-bodin@hotmail.fr>
2021-10-20 12:52:34 +0200

    corrected exo 5

--- day-1/exercise-5.js
+++ day-1/exercise-5.js
@@ -3,7 +3,7 @@ import { my_size_alpha } from "./exercise-4.js";
 export const my_array_alpha = (str) => {
   var result = [];
   for (var i = 0; i < my_size_alpha(str); i++) {
-    result.push(str[i]);
+    result[i] = str[i];
   }
   return result;
 }
